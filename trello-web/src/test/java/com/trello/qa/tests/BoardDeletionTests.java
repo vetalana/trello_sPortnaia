f0fc46387d36f156680bb07805fd1cb5768103b6
vetalana <54117600+vetalana@users.noreply.github.com>
2019-10-07 10:31:35 +0300

    111

--- trello-web/src/test/java/com/trello/qa/tests/BoardDeletionTests.java
+++ trello-web/src/test/java/com/trello/qa/tests/BoardDeletionTests.java
@@ -14,7 +14,7 @@ public class BoardDeletionTests extends TestBase {
     public void deletionBoardTest() throws InterruptedException {
         int before = app.getBoardHelper().getPersnalBoardsCount();
         app.getBoardHelper().clickOnFirstPrivateBoard();
-        Thread.sleep(10000);
+        Thread.sleep(11000);
         app.getBoardHelper().clickOnMoreButtonInBoardMenu();
         app.getBoardHelper().initBoardDeletion();
         app.getBoardHelper().confirmBoardDeletion();//.js-delete
